7392cd43571e32f2b79e5b7005beb091411a0fd1
Frank Richter <frank.richter@gmail.com>
2024-06-30 18:40:55 +0200

    Add & use hdr::DisplayID internal type

--- common/HDR.cpp
+++ common/HDR.cpp
@@ -24,6 +24,22 @@ namespace hdr {
 
 static const bool use_win11_24h2_color_functions = IsWindows11_24H2OrGreater();
 
+struct DisplayID
+{
+    LUID adapter;
+    UINT32 id;
+
+    static DisplayID FromMode(const DISPLAYCONFIG_MODE_INFO& mode)
+    {
+        return DisplayID { .adapter = mode.adapterId, .id = mode.id };
+    }
+    void ToDeviceInputHeader(DISPLAYCONFIG_DEVICE_INFO_HEADER& header) const
+    {
+        header.adapterId = adapter;
+        header.id = id;
+    }
+};
+
 template<typename F> static void ForEachDisplay(F func)
 {
     uint32_t pathCount = 0;
@@ -41,19 +57,17 @@ template<typename F> static void ForEachDisplay(F func)
     for (const auto& path : paths) {
         const auto& mode = modes.at(path.targetInfo.modeInfoIdx);
 
-        func(mode);
+        func(DisplayID::FromMode(mode));
     }
 }
 
-static Status GetDisplayHDRStatus(const DISPLAYCONFIG_MODE_INFO& mode)
+static Status GetDisplayHDRStatus(const DisplayID& display)
 {
     // Prefer GET_ADVANCED_COLOR_INFO_2, this reports the actual HDR mode if ACM is enabled
     DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO_2 getColorInfo2 = {};
     getColorInfo2.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO_2;
     getColorInfo2.header.size = sizeof(getColorInfo2);
-    getColorInfo2.header.adapterId.HighPart = mode.adapterId.HighPart;
-    getColorInfo2.header.adapterId.LowPart = mode.adapterId.LowPart;
-    getColorInfo2.header.id = mode.id;
+    display.ToDeviceInputHeader(getColorInfo2.header);
     if (use_win11_24h2_color_functions && DisplayConfigGetDeviceInfo(&getColorInfo2.header) == ERROR_SUCCESS)
     {
         if (!getColorInfo2.highDynamicRangeSupported)
@@ -66,9 +80,7 @@ static Status GetDisplayHDRStatus(const DISPLAYCONFIG_MODE_INFO& mode)
     DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO getColorInfo = {};
     getColorInfo.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_ADVANCED_COLOR_INFO;
     getColorInfo.header.size = sizeof(getColorInfo);
-    getColorInfo.header.adapterId.HighPart = mode.adapterId.HighPart;
-    getColorInfo.header.adapterId.LowPart = mode.adapterId.LowPart;
-    getColorInfo.header.id = mode.id;
+    display.ToDeviceInputHeader(getColorInfo.header);
 
     if (DisplayConfigGetDeviceInfo(&getColorInfo.header) != ERROR_SUCCESS)
         return Status::Unsupported;
@@ -84,8 +96,8 @@ Status GetWindowsHDRStatus()
     bool anySupported = false;
     bool anyEnabled = false;
 
-    ForEachDisplay([&](const DISPLAYCONFIG_MODE_INFO& mode) {
-        Status displayStatus = GetDisplayHDRStatus(mode);
+    ForEachDisplay([&](const DisplayID& display) {
+        Status displayStatus = GetDisplayHDRStatus(display);
         anySupported |= displayStatus != Status::Unsupported;
         anyEnabled |= displayStatus == Status::On;
     });
@@ -96,9 +108,9 @@ Status GetWindowsHDRStatus()
         return Status::Unsupported;
 }
 
-static std::optional<Status> SetDisplayHDRStatus(const DISPLAYCONFIG_MODE_INFO& mode, bool enable)
+static std::optional<Status> SetDisplayHDRStatus(const DisplayID& display, bool enable)
 {
-    if (GetDisplayHDRStatus(mode) == Status::Unsupported)
+    if (GetDisplayHDRStatus(display) == Status::Unsupported)
         return std::nullopt;
 
     /* Try SET_HDR_STATE first, if available (on Windows 11 >= 24H2).
@@ -107,33 +119,29 @@ static std::optional<Status> SetDisplayHDRStatus(const DISPLAYCONFIG_MODE_INFO& 
     DISPLAYCONFIG_SET_HDR_STATE setHdrState = {};
     setHdrState.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_HDR_STATE;
     setHdrState.header.size = sizeof(setHdrState);
-    setHdrState.header.adapterId.HighPart = mode.adapterId.HighPart;
-    setHdrState.header.adapterId.LowPart = mode.adapterId.LowPart;
-    setHdrState.header.id = mode.id;
+    display.ToDeviceInputHeader(setHdrState.header);
     setHdrState.enableHdr = enable;
     if (use_win11_24h2_color_functions && DisplayConfigSetDeviceInfo(&setHdrState.header) == ERROR_SUCCESS)
-        return GetDisplayHDRStatus(mode);
+        return GetDisplayHDRStatus(display);
 
     DISPLAYCONFIG_SET_ADVANCED_COLOR_STATE setColorState = {};
     setColorState.header.type = DISPLAYCONFIG_DEVICE_INFO_SET_ADVANCED_COLOR_STATE;
     setColorState.header.size = sizeof(setColorState);
-    setColorState.header.adapterId.HighPart = mode.adapterId.HighPart;
-    setColorState.header.adapterId.LowPart = mode.adapterId.LowPart;
-    setColorState.header.id = mode.id;
+    display.ToDeviceInputHeader(setColorState.header);
     setColorState.enableAdvancedColor = enable;
 
     if (DisplayConfigSetDeviceInfo(&setColorState.header) != ERROR_SUCCESS)
         return std::nullopt;
     // Don't assume changing the HDR mode was successful... re-query the status
-    return GetDisplayHDRStatus(mode);
+    return GetDisplayHDRStatus(display);
 }
 
 std::optional<Status> SetWindowsHDRStatus(bool enable)
 {
     std::optional<Status> status;
 
-    ForEachDisplay([&](const DISPLAYCONFIG_MODE_INFO& mode) {
-        auto new_status = SetDisplayHDRStatus(mode, enable);
+    ForEachDisplay([&](const DisplayID& display) {
+        auto new_status = SetDisplayHDRStatus(display, enable);
         if (!new_status)
             return;
 
@@ -154,14 +162,12 @@ std::optional<Status> ToggleHDRStatus()
     return SetWindowsHDRStatus(status == Status::Off ? true : false);
 }
 
-static const wchar_t* GetFallbackDisplayName(const DISPLAYCONFIG_MODE_INFO& mode)
+static const wchar_t* GetFallbackDisplayName(const DisplayID& display)
 {
     DISPLAYCONFIG_TARGET_BASE_TYPE target_base = {};
     target_base.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_TARGET_BASE_TYPE;
     target_base.header.size = sizeof(target_base);
-    target_base.header.adapterId.HighPart = mode.adapterId.HighPart;
-    target_base.header.adapterId.LowPart = mode.adapterId.LowPart;
-    target_base.header.id = mode.id;
+    display.ToDeviceInputHeader(target_base.header);
 
     if (ERROR_SUCCESS == DisplayConfigGetDeviceInfo(&target_base.header)) {
         if ((target_base.baseOutputTechnology != DISPLAYCONFIG_OUTPUT_TECHNOLOGY_OTHER)
@@ -176,24 +182,22 @@ std::vector<DisplayInfo> GetDisplays()
 {
     std::vector<DisplayInfo> result;
 
-    ForEachDisplay([&](const DISPLAYCONFIG_MODE_INFO& mode) {
+    ForEachDisplay([&](const DisplayID& display) {
         DisplayInfo new_disp;
 
-        new_disp.status = GetDisplayHDRStatus(mode);
+        new_disp.status = GetDisplayHDRStatus(display);
 
         DISPLAYCONFIG_TARGET_DEVICE_NAME deviceName = {};
         deviceName.header.type = DISPLAYCONFIG_DEVICE_INFO_GET_TARGET_NAME;
         deviceName.header.size = sizeof(deviceName);
-        deviceName.header.adapterId.HighPart = mode.adapterId.HighPart;
-        deviceName.header.adapterId.LowPart = mode.adapterId.LowPart;
-        deviceName.header.id = mode.id;
+        display.ToDeviceInputHeader(deviceName.header);
         if (DisplayConfigGetDeviceInfo(&deviceName.header) != ERROR_SUCCESS)
             return;
 
         if (deviceName.flags.friendlyNameFromEdid)
             new_disp.name = deviceName.monitorFriendlyDeviceName;
         else
-            new_disp.name = GetFallbackDisplayName(mode); // Seen with eg a laptop display.
+            new_disp.name = GetFallbackDisplayName(display); // Seen with eg a laptop display.
 
         result.emplace_back(std::move(new_disp));
     });
